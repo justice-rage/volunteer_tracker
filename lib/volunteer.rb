2c37485704e93bd270215c4cee639bbdcdb515c9
justicepelteir <justice.douglas@hey.com>
2022-05-21 13:21:15 -0700

    add save method

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -8,4 +8,9 @@ class Volunteer
         @project_id = attributes.fetch(:project_id)
     end
 
+    def save
+        result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id} RETURNING id;)")
+        @id = result.first.fetch("id").to_i
+    end
+
 end
