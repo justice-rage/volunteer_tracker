d7d2e625c3a56195da6182a4ec7161e722f3c8cf
justicepelteir <justice.douglas@hey.com>
2022-05-21 12:15:10 -0700

    add save method

--- lib/project.rb
+++ lib/project.rb
@@ -5,3 +5,8 @@ def initialize(attributes)
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
 end
+
+def save
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+end
