aa16c49d81922434ad90ab9fec026e588140a21b
justicepelteir <justice.douglas@hey.com>
2022-05-21 20:08:17 -0700

    add test passing code

--- lib/project.rb
+++ lib/project.rb
@@ -1,53 +1,53 @@
 class Project
     attr_reader :id, :title
-
+  
     def initialize(attributes)
-        @title = attributes.fetch(:title)
-        @id = attributes.fetch(:id)
+      @title = attributes.fetch(:title)
+      @id = attributes.fetch(:id)
     end
-
+  
     def save
-        result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
-        @id = result.first().fetch("id").to_i
+      result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+      @id = result.first().fetch("id").to_i
     end
-
-    def self.all
-        returned_projects = DB.exec("SELECT * FROM projects;")
-        projects = []
-        returned_projects.each() do |project|
-            title = project.fetch("title")
-            id = project.fetch("id").to_i
-            projects.push(Project.new({:title => title, :id =>id}))
-        end
-        projects
-    end
-
-    def self.find(id)
-        project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
-        if project
-            title = project.fetch("title")
-            id = project.fetch("id").to_i
-            Project.new({:title => title, :id => id})
-        else
-            nil
-        end
-    end
-
-    def ==(project_to_compare)
-        self.title() == project_to_compare.title()
-    end
-
-    def update(title)
-        @title = title
-        DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
-    end
-
+    
     def delete
-        DB.exec("DELETE FROM projects WHERE id = #{id};")
+      DB.exec("DELETE FROM projects WHERE id = #{id};")
     end
-
+    
+    def self.all
+      returned_projects = DB.exec("SELECT * FROM projects;")
+      projects = []
+      returned_projects.each() do |project|
+        title = project.fetch("title")
+        id = project.fetch("id").to_i
+        projects.push(Project.new({:title => title, :id =>id}))
+      end
+      projects
+    end
+  
+    def ==(project_to_compare)
+      self.title() == project_to_compare.title()
+    end
+  
+    def self.find(id)
+      project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+      if project
+        title = project.fetch("title")
+        id = project.fetch("id").to_i
+        Project.new({:title => title, :id => id})
+      else
+        nil
+      end
+    end
+  
+    def update(title)
+      @title = title
+      DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+    end
+  
     def volunteers
-        Volunteer.find_by_project(self.id)
+      Volunteer.find_by_project(self.id)
     end
-
-end
+  
+  end
